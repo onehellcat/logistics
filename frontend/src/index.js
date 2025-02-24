ed722201cb91d02e453ec01d9d0eca1de5af106e
onehellcat <43723078+onehellcat@users.noreply.github.com>
2025-02-24 19:35:19 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-//import "./index.css"; // Optional if you have styles
+
 
 ReactDOM.render(
   <React.StrictMode>
